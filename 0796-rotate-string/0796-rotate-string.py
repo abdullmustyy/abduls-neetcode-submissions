d9cf3769b838f8750507e1bddeb0525a2741d8f6
Abdul <abdulmusty03@outlook.com>
2026-05-04 12:58:21 +0100

    Time: 0 ms (100%), Space: 19.3 MB (51.42%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.py
+++ 0796-rotate-string/0796-rotate-string.py
@@ -1,21 +1,13 @@
 class Solution:
     def rotateString(self, s: str, goal: str) -> bool:
-        if len(s) != len(goal):
-            return False
+        s_conc = s + s
 
-        s_rot = s
-
-        for _ in range(len(s)):
-            if s_rot == goal:
-                return True
-            s_rot = s_rot[1:] + s_rot[0]
-
-        return False
+        return len(s) == len(goal) and goal in s_conc
 
 """
-- define a variable to track the rotation
-- iterate over s, compare the equality of s and goal
-- remove the current char of s in from the stored rotation
-and add it to the end of s
+- i learned that if we concatenate s to it self, then
+it will consist of every possible rotation occurrence
+- check whether goal is in the concatenated string and
+return the boolean result
 
 """
